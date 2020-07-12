1456f7fe7934d67751124ecfecd4af7288ac6f06
qbs0005 <yuuta.kawano@qdenbs.com>
2020-07-12 21:42:13 +0900

    作成中

--- app/src/main/java/jp/techacademy/yuuta/qa_app/LoginActivity.kt
+++ app/src/main/java/jp/techacademy/yuuta/qa_app/LoginActivity.kt
@@ -1,12 +1,173 @@
-package jp.techacademy.yuuta.qa_app
+package jp.techacademy.taro.kirameki.qa_app
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_login.*
+
+import java.util.HashMap
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mCreateAccountListener: OnCompleteListener<AuthResult>
+    private lateinit var mLoginListener: OnCompleteListener<AuthResult>
+    private lateinit var mDataBaseReference: DatabaseReference
+
+    // アカウント作成時にフラグを立て、ログイン処理後に名前をFirebaseに保存する
+    private var mIsCreateAccount = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mDataBaseReference = FirebaseDatabase.getInstance().reference
+
+        // FirebaseAuthのオブジェクトを取得する
+        mAuth = FirebaseAuth.getInstance()
+
+        // アカウント作成処理のリスナー
+        mCreateAccountListener = OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // 成功した場合
+                // ログインを行う
+                val email = emailText.text.toString()
+                val password = passwordText.text.toString()
+                login(email, password)
+            } else {
+
+                // 失敗した場合
+                // エラーを表示する
+                val view = findViewById<View>(android.R.id.content)
+                Snackbar.make(view, "アカウント作成に失敗しました", Snackbar.LENGTH_LONG).show()
+
+                // プログレスバーを非表示にする
+                progressBar.visibility = View.GONE
+            }
+        }
+
+        // ログイン処理のリスナー
+        mLoginListener = OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // 成功した場合
+                val user = mAuth.currentUser
+                val userRef = mDataBaseReference.child(UsersPATH).child(user!!.uid)
+
+                if (mIsCreateAccount) {
+                    // アカウント作成の時は表示名をFirebaseに保存する
+                    val name = nameText.text.toString()
+
+                    val data = HashMap<String, String>()
+                    data["name"] = name
+                    userRef.setValue(data)
+
+                    // 表示名をPrefarenceに保存する
+                    saveName(name)
+                } else {
+                    userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val data = snapshot.value as Map<*, *>?
+                            saveName(data!!["name"] as String)
+                        }
+
+                        override fun onCancelled(firebaseError: DatabaseError) {}
+                    })
+                }
+
+                // プログレスバーを非表示にする
+                progressBar.visibility = View.GONE
+
+                // Activityを閉じる
+                finish()
+
+            } else {
+                // 失敗した場合
+                // エラーを表示する
+                val view = findViewById<View>(android.R.id.content)
+                Snackbar.make(view, "ログインに失敗しました", Snackbar.LENGTH_LONG).show()
+
+                // プログレスバーを非表示にする
+                progressBar.visibility = View.GONE
+            }
+        }
+
+        // UIの準備
+        title = "ログイン"
+
+        createButton.setOnClickListener { v ->
+            // キーボードが出てたら閉じる
+            val im = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            im.hideSoftInputFromWindow(v.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+
+            val email = emailText.text.toString()
+            val password = passwordText.text.toString()
+            val name = nameText.text.toString()
+
+            if (email.length != 0 && password.length >= 6 && name.length != 0) {
+                // ログイン時に表示名を保存するようにフラグを立てる
+                mIsCreateAccount = true
+
+                createAccount(email, password)
+            } else {
+                // エラーを表示する
+                Snackbar.make(v, "正しく入力してください", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+        loginButton.setOnClickListener { v ->
+            // キーボードが出てたら閉じる
+            val im = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            im.hideSoftInputFromWindow(v.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+
+            val email = emailText.text.toString()
+            val password = passwordText.text.toString()
+
+            if (email.length != 0 && password.length >= 6) {
+                // フラグを落としておく
+                mIsCreateAccount = false
+
+                login(email, password)
+            } else {
+                // エラーを表示する
+                Snackbar.make(v, "正しく入力してください", Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        // プログレスバーを表示する
+        progressBar.visibility = View.VISIBLE
+
+        // アカウントを作成する
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(mCreateAccountListener)
+    }
+
+    private fun login(email: String, password: String) {
+        // プログレスバーを表示する
+        progressBar.visibility = View.VISIBLE
+
+        // ログインする
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(mLoginListener)
+    }
+
+    private fun saveName(name: String) {
+        // Preferenceに保存する
+        val sp = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = sp.edit()
+        editor.putString(NameKEY, name)
+        editor.commit()
     }
 }
--- app/src/main/java/jp/techacademy/yuuta/qa_app/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuuta/qa_app/MainActivity.kt
@@ -1,28 +1,42 @@
 package jp.techacademy.yuuta.qa_app
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.FloatingActionButton
 import android.view.Menu
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.content.Intent
+import android.support.v7.widget.Toolbar
+import jp.techacademy.taro.kirameki.qa_app.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        mToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(mToolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        // --- ここから ---
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { _ ->
+            // ログイン済みのユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
+
+            // ログインしていなければログイン画面に遷移させる
+            if (user == null) {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
