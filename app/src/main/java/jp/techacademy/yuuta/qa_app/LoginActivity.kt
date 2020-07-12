f80b96ebe705efaefc418241f8b1f63fab34f6d3
qbs0005 <yuuta.kawano@qdenbs.com>
2020-07-12 21:26:10 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yuuta/qa_app/LoginActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.yuuta.qa_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
